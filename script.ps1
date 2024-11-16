301dd4714810db3c84e39013307767aabdb35458
master135 <master135@ua.fm>
2024-11-16 20:54:54 +0000

    Added upload to artifacts

--- script.ps1
+++ script.ps1
@@ -1,6 +1,6 @@
 # Define log file
-$logFile = "extended_logs.txt"
+$logFile = $env:Missing_users
 # Write to the console
 Write-Output "This is critical information"
 # Write extended logs to a file
-"This is an extended log" | Out-File -FilePath $logFile -Append
+$logFile | Out-File -FilePath $logFile -Append
