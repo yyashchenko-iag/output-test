7490465a7cce136b7b7c3df38d96fd766f887598
master135 <master135@ua.fm>
2024-11-17 13:23:53 +0000

    added groups

--- script.ps1
+++ script.ps1
@@ -1,8 +1,9 @@
 # Define log file
 $logFile_users = $env:Missing_users
+$logFile_groups = $env:Missing_groups
 $list = Import-Csv -Path .\users.csv
 # Write to the console
 Write-Output "This is critical information"
 # Write extended logs to a file
 Write-Output "This is critical information" | Out-File -FilePath $logFile_users -Append
-$list.role | Out-File -FilePath $logFile_groups -Append
+$list.role | foreach $_ | Out-File -FilePath $logFile_groups -Append
